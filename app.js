fc51ee009cec3b0dc6f4f9fd412622fb91cfdee9
Obinna Odirionye <odirionye@gmail.com>
2019-03-10 13:57:54 +0100

    chore(app.js): changed the body parser to express
    as of express@4.15+, there is no need to use body parser. express now
    ships with its inbuit body parser.

--- app.js
+++ app.js
@@ -25,8 +25,8 @@ app.set('view engine', 'pug'); // we use the engine pug, mustache or EJS work gr
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Takes the raw requests and turns them into usable properties on req.body
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // Exposes a bunch of methods for validating data. Used heavily on userController.validateRegister
 app.use(expressValidator());
