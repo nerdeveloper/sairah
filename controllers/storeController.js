0796ff1f2369f5ac0eaf6c9de465c35f6d138def
Obinna Odirionye <odirionye@gmail.com>
2019-03-13 19:55:52 +0100

    chore(storecard): added an edit button

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -12,7 +12,8 @@ exports.addStore = (req, res) => {
 exports.createStore = async (req, res) => {
     const store = await (new Store(req.body)).save();
     req.flash('success', `Successfully Created ${store.name}. Care to leave a review?`)
-    res.redirect(`/store/${store.slug}`); 
+   // res.redirect(`/store/${store.slug}`); 
+    res.redirect(`/stores`); 
     
 };
 
