50bd63846326ac474b79cec29ff44978b6aeeb71
Obinna Odirionye <odirionye@gmail.com>
2019-03-14 10:13:54 +0100

    chore(models): update store model

--- models/store.js
+++ models/store.js
@@ -15,7 +15,25 @@ const storeSchema = new mongoose.Schema({
         trim: true,
         required: "Please enter a description for youu Store." 
     },
-    tags: [String]
+    tags: [String],
+    created: {
+        type: Date,
+        default: Date.now
+    },
+    location: {
+        type: {
+            type: String,
+            default: 'Point'
+        },
+        coordinates: [{
+            type: Number,
+            required: 'You must supply coordinates!'
+        }],
+        address: {
+            type: 'String',
+            required: 'You must supply an address!'
+        }
+    }
 });
 
 storeSchema.pre('save', function (next){
