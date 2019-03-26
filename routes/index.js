2867caf25852eb8026cac3959198b30ea1ce751d
Obinna Odirionye <odirionye@gmail.com>
2019-03-26 13:43:53 +0100

    created a user controller and route

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const storeController  = require('../controllers/storeController')
+const userController = require('../controllers/userController');
 const { catchErrors } =  require('../handlers/errorHandlers')
 
 // Do work here
@@ -25,5 +26,8 @@ router.get('/store/:slug', catchErrors(storeController.getStoreBySlug));
 
 router.get('/tags', catchErrors(storeController.getStoresByTag));
 router.get('/tags/:tag', catchErrors(storeController.getStoresByTag));
+
+router.get('/login', userController.loginForm);
+
 
 module.exports = router;
